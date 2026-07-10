17d0a2120b162a3b8b50826a03aa341290e3eeaf
탭・TAB・タブ <me@tabthedev.kr>
2026-07-10 10:11:40 +0900

    모든 SwitchCamera 이벤트 구역 개체가 공통된 물리 충돌을 가지게끔 스크립트에서 기본값 설정

--- Assets/Scripts/LocalPlayer/Camera/SwitchCamera.cs
+++ Assets/Scripts/LocalPlayer/Camera/SwitchCamera.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class SwitchCamera : MonoBehaviour
 {
+    private Collider2D collider;
     private GameObject player;
     private Camera camera;
 
@@ -9,6 +10,16 @@ public class SwitchCamera : MonoBehaviour
     {
         player = GameObject.FindGameObjectWithTag("Player");
         camera = GameObject.FindAnyObjectByType<Camera>();
+
+        LayerMask layer_nothing = LayerMask.GetMask();
+        LayerMask layer_localPlayer = LayerMask.GetMask("LocalPlayer");
+
+        collider.includeLayers = layer_nothing;
+        collider.excludeLayers = layer_nothing;
+        collider.forceSendLayers = layer_nothing;
+        collider.forceReceiveLayers = layer_nothing;
+        collider.contactCaptureLayers = layer_localPlayer;
+        collider.callbackLayers = layer_localPlayer;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
